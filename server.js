4af96bd49973781b11e6d4a1e6854f6d292309dc
jenrus <jenrus@tuta.io>
2019-04-13 15:45:01 +0300

    remove debug output

--- server.js
+++ server.js
@@ -5,18 +5,15 @@ server = http.createServer(app),
 io = require('socket.io').listen(server);
 app.get('/', (req, res) => {
 
-// res.send('SnakeChat server is running')
+res.send('SnakeChat server is running')
 });
 io.on('connection', (socket) => {
 
 socket.on('join', function(userNickname) {
-        // console.log(userNickname +" has joined the chat "  );
         socket.broadcast.emit('userjoinedthechat',userNickname +" has joined the chat ");
     });
 
 socket.on('messagedetection', (senderNickname,messageContent) => {
-    //log the message in console 
-    // console.log(senderNickname+" : " +messageContent)
     //create a message object 
     let  message = {"message":messageContent, "senderNickname":senderNickname}
     // send the message to the client side  
@@ -30,5 +27,4 @@ socket.on('disconnect', function() {
 });
 
 server.listen(3000,()=>{
-// console.log('SnakeChat read-only mode:');
 });
